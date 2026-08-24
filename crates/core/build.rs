d1ee3b86042099c37f1d46e966db7d1d187df138
Liu Zhening <129078862+HYPERVAPOR@users.noreply.github.com>
2026-08-24 19:34:29 +0800

    Move core crate into crates/ directory

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../proto");
+    let proto_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../../proto");
     let proto_file = proto_dir.join("myb.proto");
 
     println!("cargo:rerun-if-changed={}", proto_file.display());
